37649c7ea34e9ba160ddf41420bc45a392504b8f
Amine Sahibi <amineshaibi@gmail.com>
2011-11-19 01:57:02 -0500

    Added the dbConnect.php file for connection with MySQl

---
+++ dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+    //constants
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'amine';
+    $dbDatabase = 'mydb';
+    //make connection
+    $server = mysql_connect($dbHost, $dbUsername, $dbPassword);
+    $connection = mysql_select_db($dbDatabase, $server);
+?>
