bbbd1a3c5876c5962d6af79711ae967a6833b68f
Amine Sahibi <amineshaibi@gmail.com>
2011-11-19 03:43:17 -0500

    postorder update with working SQL command

--- postorder.php
+++ postorder.php
@@ -1,43 +1,56 @@
 <?php
-$postOrder = true; 
 
-require_once 'StockExchangePackage.php';
+ include_once('dbConnect.php');
+ include_once ('Order.php');
 
 /* 
  * if message is not identified as an error, kill the 
  * process and return an error
  */
-
+ 
 
 /*
  * need to generate an XML respons
  */
-$msgType = $_Post["MessageType"];
+$msgType = $_POST["MessageType"];
 if(strcmp($msgType, "O")!=0 )
 {
     die('Message did not identify as an order!');
 }
+
 /*
  * Load data from http post request
  */
-$from = $_Post["From"];
-$bs = $_Post["BS"];
-$shares = $_Post["Shares"];
-$stock = $_Post["Stock"];
-$price= $_Post["Price"]; 
-$twilio= $_Post["Twilio"];
+$from = $_POST["From"];
+$bs = $_POST["BS"];
+$shares = $_POST["Shares"];
+$stock = $_POST["Stock"];
+$price= $_POST["Price"];
+$twilio= $_POST["Twilio"];
 $state = 'U';
 
-
+if($twilio=='N'){
+    $twilio=false;
+}
+else{
+    $twilio=true;
+}
 /*
  * Create a new order
  */
-$order =new Order($from, $bs, $shares, $stock, $price, $twilio, $state);
+
+/*$order =new Order($from, $bs, $shares, $stock, $price, $twilio, $state);
 $order->isValid();
 $order->insertToPending();
+$s=$order->toString();*/
 
-include('processor.php');
-processTransactions();
+$q1="INSERT INTO order_book_pending (`from`, `bs`, `shares`, `stock`, `price`, `twilio`, `timestamp`,`state`)
+	VALUES ('$from', '$bs', '$shares', '$stock', '$price', '$twilio', CURRENT_TIMESTAMP, 'U')";
+//$q = "INSERT into test (line) values ('$price')";
+
+$query = mysql_query($q);
+//include('processor.php');
+//processTransactions();
 
 //mysql_close();
 ?>
