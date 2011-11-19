e07654d4b56215bebceb8554fcdba4152d554f2e
unknown <Omega@Omega-Laptop.(none)>
2011-11-19 06:41:35 -0500

    processor lock works, only one process will spawn -faiz

--- processor.php
+++ processor.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once('dbConnect.php');
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,6 +7,45 @@
 
 function processTransactions ()
 {
+    // if the process isn't running
+    if (processorIsRunning()==1)
+    {
+        //try to lock it
+        if (lockProcessor() == 1)
+        {
 
+            
+    
+    
+        }            
+    }
 }
+
+function lockProcessor()
+{
+    $query = "SELECT GET_LOCK('Singleton',5)";
+    $result = mysql_query($query);
+    
+    $check = mysql_fetch_row($result);
+    
+    return $check[0];
+}
+
+function processorIsRunning()
+{
+    
+    $query = "SELECT IS_FREE_LOCK('Singleton')";
+    $result = mysql_query($query);
+    
+    $check = mysql_fetch_row($result);
+    
+    $fh = fopen('wow.txt', 'w+');
+    fwrite($fh, $check[0]);
+    fclose($fh);
+    
+    return $check[0];
+    
+}
+
+processTransactions();
 ?>
