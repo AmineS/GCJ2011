936bacc2d3a926677ef82335ec71202abb592f3d
Amine Sahibi <amineshaibi@gmail.com>
2011-11-19 23:02:10 -0500

    Faiz added post.php

---
+++ post.php
@@ -0,0 +1,18 @@
+<?php
+
+function postToBroker($brokerAddress, $brokerPort, $brokerEndPoint, $response)
+{
+    $url = 'http://' . $brokerAddress . ':'. $brokerPort .'/'. $brokerEndPoint;
+    $r = new HttpRequest($url, HttpRequest::METH_POST);
+    $r->addRawPostData($response);
+    $r->setContentType("Content-Type: text/xml");
+
+    try {
+        echo $r->send()->getBody();
+    } catch (HttpException $ex) {
+        echo $ex;
+    }
+    die($response);
+}
+
+?>
