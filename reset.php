85d4e87ea06072450d0cc2447f410ef70ed3c788
Amine Sahibi <amineshaibi@gmail.com>
2011-11-20 01:17:45 -0500

    shita

--- reset.php
+++ reset.php
@@ -1,26 +1,8 @@
 <?php
 
 /*
- * reset simply truncates every table in the database to have no values
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-
-include_once('dbConnect.php');
-$link = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
-$query = "Truncate Table order_book_pending;";
-$query .= "Truncate Table order_book_active;";
-$query .= "Truncate Table order_book_archive;";
-$query .= "Truncate Table trade_book;";
-$query .= "Truncate Table process_singleton_lock;";
-
-$result = mysqli_multi_query($link, $query);
-if($result)
-{
-    echo "went trough";
-    return 1;
-}
-else
-{
-    echo mysql_error();//return 0;*/
-}
-
+echo('hooooooo');
 ?>
